8c666974d7ce277c99fc575e1e2394f9a4253e5a
Paul Dkn <pauldkn.code@gmail.com>
2020-03-10 10:34:01 +0100

    First commit

---
+++ src/views/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Hello, my name is ... and you are on my solution for the Ironhack challenge!</p>
+    </div>
+  )
+}
